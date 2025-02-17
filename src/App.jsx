62bd39a7ac428d43986999cb996503c47625d9da
rishamm <muhammedrisham45334@gmail.com>
2025-02-17 18:13:01 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import Spline from '@splinetool/react-spline';
-import { useEffect, useState } from 'react';
-
 export default function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    const enableWebGL = () => {
-      document.body.removeEventListener("touchstart", enableWebGL);
-      setIsLoaded(true);  // Set state once touch event triggers WebGL enable
-    };
-
-    document.body.addEventListener("touchstart", enableWebGL, false);
-  }, []);
+  
 
   return (
     <div className="w-full" style={{ width: "100%" }}>
-      {isLoaded ? (
+
         <Spline scene="https://prod.spline.design/dxHvoztIFiobRBsO/scene.splinecode" />
-      ) : (
-        <div>Loading 3D scene...</div> // Show loading message
-      )}
+     
     </div>
   );
 }
