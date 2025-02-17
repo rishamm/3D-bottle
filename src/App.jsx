e9ebb04042ad18a1bea03858c1eb4fd9462e203f
rishamm <muhammedrisham45334@gmail.com>
2025-02-17 15:52:54 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import React, { lazy, Suspense } from "react";
-const Spline = lazy(() => import("@splinetool/react-spline"));
+import Spline from '@splinetool/react-spline'
 
 export default function App() {
   return (
     <div className="w-full"style={{width:"100%"}}>
-
-
-    <Suspense fallback={<div>Loading...</div>}>
       <Spline scene="https://prod.spline.design/dxHvoztIFiobRBsO/scene.splinecode" />
-    </Suspense></div>
+   </div>
   );    
 }
