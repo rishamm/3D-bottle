f8d5600c704742d2cbb10fc40d85c40838402cdb
rishamm <muhammedrisham45334@gmail.com>
2025-02-17 20:44:28 +0530

    bug fixs

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-import Spline from "@splinetool/react-spline";
-import { useEffect, useState } from "react";
-
-export default function ResponsiveSpline() {
-  const [splineScene, setSplineScene] = useState(null);
-
-  useEffect(() => {
-    if (!splineScene) return;
-
-    const adjustCameraZoom = () => {
-      if (!splineScene) return;
-      const width = window.innerWidth;
-
-      if (width <= 768) {
-        splineScene.setZoom(0.8); // Adjust zoom for mobile
-      } else {
-        splineScene.setZoom(1); // Normal zoom for desktop
-      }
-    };
-
-    window.addEventListener("resize", adjustCameraZoom);
-    adjustCameraZoom();
-
-    return () => window.removeEventListener("resize", adjustCameraZoom);
-  }, [splineScene]);
+import Spline from '@splinetool/react-spline';
+export default function App() {
+  
 
   return (
-    <div className="spline-container">
-      <Spline
-        scene="https://prod.spline.design/p4jYwZfhIsFf15sH/scene.splinecode"
-        onLoad={(scene) => setSplineScene(scene)}
-      />
+    <div className="w-full" style={{ width: "100%" }}>
+
+        <Spline scene="https://prod.spline.design/N6-6tBejIp7rn6pw/scene.splinecode" />
+     
     </div>
   );
 }
