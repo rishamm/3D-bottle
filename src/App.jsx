6045cedc6e1c7d677baa1d1b796ce8043acc1292
rishamm <muhammedrisham45334@gmail.com>
2025-02-17 18:58:03 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="w-full" style={{ width: "100%" }}>
 
-        <Spline scene="https://prod.spline.design/N6-6tBejIp7rn6pw/scene.splinecode" />
+        <Spline scene="https://prod.spline.design/dxHvoztIFiobRBsO/scene.splinecode" />
      
     </div>
   );
