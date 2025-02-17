c610e465f7ed157e5c5c868ec779095ae36def65
rishamm <muhammedrisham45334@gmail.com>
2025-02-17 16:07:56 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Spline from '@splinetool/react-spline'
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {
+    const enableWebGL = () => {
+      document.body.removeEventListener("touchstart", enableWebGL);
+    };
+    document.body.addEventListener("touchstart", enableWebGL, false);
+  }, []);
   return (
-    <div className="w-full"style={{width:"100%"}}>
-      <Spline scene="https://prod.spline.design/dxHvoztIFiobRBsO/scene.splinecode" />
+    <div className="w-full"style={{width:"100%"}}   >
+      <Spline scene="https://prod.spline.design/dxHvoztIFiobRBsO/scene.splinecode"  />
    </div>
   );    
 }
